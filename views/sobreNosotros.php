76b4df5f5aceb1197a96ea3c6dd77c1fd4281f5b
Alejandro <yoomin7@hotmail.com>
2022-11-01 01:41:45 -0300

    nosotro nomais

--- views/sobreNosotros.php
+++ views/sobreNosotros.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../estilos/estilos.css">
-    <title>Hamburguesas</title>
+    <title>Sobre Nosotros</title>
 </head>
 
 <body class="fondo-imagen">
@@ -16,13 +16,34 @@
         ?>
     </header>
 
+    <center><img src="../img/anvorguesa2.png" alt="" class="imagensn"></center>
 
- 
+    <center><h1 class="lobster sntamanio">       
+        Sobre Nosotros
+    </h1></center>
+        <div class="container">
+            <p class="espaciado">
+            Anvorguesa nace en Buenos Aires, Argentina, con la premisa de ofrecer hamburguesas y hot dogs de calidad premium, con locales de ambiente moderno, distendido y agradable, diseñados en detalle.
+            <br>
+            Contamos con nuestra propia planta de producción, donde fabricamos hamburguesas de 100% carne premium de novillo, medallones de pollo y veggie, entre otros. Utilizamos los mejores insumos del mercado, exclusivos y customizados. Nuestros proveedores son grandes socios estratégicos y comparten nuestros valores.
+            <br>
+            Pioneros del concepto FAST GOOD, nuestra oferta está diseñada para superar las expectativas de los diversos consumidores actuales. La calidad integrada al concepto de fast food conforma nuestra visión: somos el fast food de la nueva generación.
+            <br>
+            Hoy contamos con más de 25 locales en Argentina, y recientemente abrimos en Uruguay con una proyección de 5 sucursales. En los próximos años, proyectamos el desembarco en Colombia, México, Paraguay, Chile, Ecuador, España, y EEUU. 
+            <br>
+            Esperamos satisfacer al mercado mundial de hamburguesas ofreciendo calidad y una nueva experiencia FAST GOOD.
+            </p>
+        </div>
+    <div>
+        <img src="../img/fooddibujo.png" class="img-fluid" alt="...">
+    </div>
+        
+
     <footer class="footer">
        <?php
             require_once('footer.php')
         ?>
-    </footer>
+    </footer class="footer">
 
 </body>
 
