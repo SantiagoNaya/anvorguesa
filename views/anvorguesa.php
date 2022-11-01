b9760c08da17e4d0601ac3205e903b05ba453c54
santiago <54956952+SantiagoNaya@users.noreply.github.com>
2022-10-31 23:02:38 -0300

    Pagina de productos de anvorguesa lista
    modifique el css para la pagina de anvorguesa

--- views/anvorguesa.php
+++ views/anvorguesa.php
@@ -15,6 +15,62 @@
            require_once('header2.php');
         ?>
     </header>
+
+
+ 
+<center><h1 > Nuestras Mejores Anvorrrrrguesas  </h1></center>
+
+
+   
+<div class="news-cards"></div>
+    <div class="news-cards">
+
+     <img src="../img/kebab-1.png" alt="hamburgesa1" >
+     <h2>Hamburguesa kebab</h2>
+    
+     <p>Hamburguesa de pollo con jamon queso tomate lechuga.</p>
+     
+
+     </div>
+
+     <div class="news-cards">
+
+<img src="../img/hamburger1.png" alt="hamburgesa2" >
+<h2>Hamburguesa clasica</h2>
+<p>Hamburguesa con lechuga tomate pepino cebolla queso chedar y una salsa muy especial.</p>
+
+
+</div>
+
+<div class="news-cards">
+
+<img src="../img/hamburger2.png" alt="hamburgesa3" >
+<h2>Hamburguesa simple</h2>
+<p>Hamburguesa con pepino doble queso chedar cebolla mayonesa y kechup.</p>
+
+
+</div>
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <footer class="footer">
        <?php
             require_once('footer.php')
